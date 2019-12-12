4a5b130d62066d00aff4102ffd3ce2c2a1daee23
ErikDenis <33569321+ErikDenis@users.noreply.github.com>
2019-12-12 22:29:11 +0300

    Update custom.js

--- _static/js/custom.js
+++ _static/js/custom.js
@@ -1,10 +1,10 @@
 const SelAll = document.querySelectorAll;
 
-if (SelAll('.rst-content img').style.height >= '150px')
+if (SelAll('.rst-content img element.style').style.height >= '150px')
 {
-    SelAll('.rst-content img').style.width = 'auto';
+    SelAll('.rst-content img element.style').style.width = 'auto';
 }
 else
 {
-    SelAll('.rst-content img').style.width = '100%';
+    SelAll('.rst-content img element.style').style.width = '100%';
 }
