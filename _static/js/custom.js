676459ae315276f1b807155e87de4409df2ec435
ErikDenis <erik.denis.bauer@mail.ru>
2019-12-12 23:37:03 +0300

    0

---
+++ _static/js/custom.js
@@ -0,0 +1,10 @@
+const SelAll = document.querySelectorAll;
+
+if (SelAll('.rst-content img element.style').style.height >= '150px')
+{
+    SelAll('.rst-content img element.style').style.width = 'auto';
+}
+else
+{
+    SelAll('.rst-content img element.style').style.width = '100%';
+}
