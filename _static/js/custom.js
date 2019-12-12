91513c5b02453143be98c9a3a02752d01ccee0ec
ErikDenis <33569321+ErikDenis@users.noreply.github.com>
2019-12-12 19:24:45 +0300

    Update custom.js

--- _static/js/custom.js
+++ _static/js/custom.js
@@ -1,8 +1,9 @@
-if (('#reference internal image-reference img').style.height() >= '150px')
-{
-    ('#reference internal image-reference img').style.width() = 'auto';
-}
-else
-{
-    ('#reference internal image-reference img').style.width() = '100%';
-}
+// if (('#reference internal image-reference img').style.height() >= '150px')
+// {
+//     ('#reference internal image-reference img').style.width() = 'auto';
+// }
+// else
+// {
+//     ('#reference internal image-reference img').style.width() = '100%';
+// }
+('.rst-content img').style.height = '5px';
