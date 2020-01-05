ae7c3e511cc66797d258ed4490b913fc0885e746
ErikDenis <erik.denis.bauer@mail.ru>
2020-01-05 14:25:21 +0300

    Upgraded

--- conf.py
+++ conf.py
@@ -44,7 +44,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'Rainmeter Документация'
+project = 'Rainmeter'
 copyright = 'Rainmeter (translate by ErikDenis)'
 
 
